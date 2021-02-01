7d34a8d449c3e8345e1ba33fbd75a9f8ea302f1b
miablo <mvd5044@psu.edu>
2021-02-01 09:38:38 -0500

    Added methods to root class

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,9 @@ public class Main {
 (4 pts) Implement a Java program that applies the Newton-Raphson's method xn+1 = xn – f(xn) / f '(xn)
 to search the roots for this polynomial function ax6 – bx5 + cx4 – dx3+ ex2 – fx + g = 0. Fill out a, b, c, d, e, f, and g
 using the first 7 digits of your PSU ID, respectively. For example, if PSU ID is 987654321,
-the polynomial function would be 9x6 – 8x5 + 7x4 – 6x3+ 5x2 – 4x + 3 = 0. The program terminates when the difference between the new solution
+the polynomial function would be 9x6 – 8x5 + 7x4 – 6x3+ 5x2 – 4x + 3 = 0.
+
+The program terminates when the difference between the new solution
 and the previous one is smaller than 0.00001 within 2000 iterations. Otherwise, it shows Not Found as the final solution.
 
 Your team’s goal is to find as many roots as possible. For the Newton-Raphson’s method, you will need to start with a guessed x0
--- src/Root.java
+++ src/Root.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  *
  * @author Mio Diaz,
@@ -5,5 +7,33 @@
  *
  */
 public class Root {
+    // Newton-Raphson's method xn+1 = xn – f(xn) / f '(xn)
+    int x_0 = randX();
+    int x_next, x_prev;
+
+
+
+
+
+    // start with a guessed x0
+    //value which can be randomly generated.
+    /**
+     * Random number generator for initial x value
+     *
+     * @return random value for x_0
+     */
+    public int randX(){
+        Random rand = new Random();
+        return rand.nextInt();
+    }
+
+    /**
+     * Boolean returns true if root is found else false
+     *
+     * @return true if root found; false if not
+     */
+    public boolean isRoot() {
+        return false;
+    }
 
 }
