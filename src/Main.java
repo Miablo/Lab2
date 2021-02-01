c025ef84fdfb29b5db5d577e956ce5dd9acc42e4
Pcow9 <71295830+Pcow9@users.noreply.github.com>
2021-02-01 11:13:19 -0500

    Finish Math equation and main for Cody Id. Made a commented scetion for Mio to add hers and run Codys has no solutions

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
- * @author Mio Diaz,
+ * @author Mio Diaz, Cody Walker
  * @version 1.0
  *
  */
 public class Main {
+
+    static Random rand = new Random();
+    static ArrayList x = new ArrayList();
+    static Root root = new Root();
+    static DecimalFormat decFor = new DecimalFormat("#0.00000");
+    static int i = 0;
+    static int rootFound = 0;
+    static int min = 0;
+    static int max = 100;
 
     /**
      *  Main Method
@@ -12,8 +25,54 @@ public class Main {
      * @param args not used
      */
     public static void main(String[] args) {
+        // for Cody's PSU ID
+        root.setNumbers(9,5,5,0,7,4,1);
+        int j = 0;
+        do {
+            i = 0;
+            x.add((double) rand.nextDouble() * (max - min +1) + min);
+            do {
+                x.add(root.doMath((Double) x.get(i)));
+                i++;
+            } while (i < 2000 || decFor.format((double) x.get(i - 1)) == decFor.format((double) x.get(i) + 0.00001));
 
+            System.out.println("For Cody x0 = " + decFor.format(x.get(0)));
+            if (decFor.format((double) x.get(i - 1)) == decFor.format((double) x.get(i) + 0.00001)) {
+                System.out.println("Iterations: x" + x.size() + " The number is" + decFor.format((double) x.get(i)));
+                rootFound++;
+            } else {
+                System.out.println("Not Found as the final solution");
+            }
+            x.clear();
+            j++;
+        }while(j<2000);
+        System.out.println("Number of root Found for Cody: " + rootFound);
     }
+    /**
+     * mio add you psu Id to setNumbers and un comment everything
+     * root.setNumbers(9,5,5,0,7,4,1);
+     *         int j = 0;
+     *         do {
+     *             i = 0;
+     *             x.add((double) rand.nextDouble() * (max - min +1) + min);
+     *             do {
+     *                 x.add(root.doMath((Double) x.get(i)));
+     *                 i++;
+     *             } while (i < 2000 || decFor.format((double) x.get(i - 1)) == decFor.format((double) x.get(i) + 0.00001));
+     *
+     *             System.out.println("For Cody x0 = " + decFor.format(x.get(0)));
+     *             if (decFor.format((double) x.get(i - 1)) == decFor.format((double) x.get(i) + 0.00001)) {
+     *                 System.out.println("Iterations: x" + x.size() + " The number is" + decFor.format((double) x.get(i)));
+     *                 rootFound++;
+     *             } else {
+     *                 System.out.println("Not Found as the final solution");
+     *             }
+     *             x.clear();
+     *             j++;
+     *         }while(j<2000);
+     *         System.out.println("Number of root Found for Cody: " + rootFound);
+     *     }
+     */
 }
 
 /*
--- src/Root.java
+++ src/Root.java
@@ -2,17 +2,24 @@ import java.util.Random;
 
 /**
  *
- * @author Mio Diaz,
+ * @author Mio Diaz, Cody Walker
  * @version 1.0
  *
  */
 public class Root {
     private double a, b, c, d, e, f, g, x;
     private double equ;
-    // Newton-Raphson's method xn+1 = xn – f(xn) / f '(xn)
-    int x_0 = randX();
-    int x_next, x_prev;
 
+    /**
+     *
+     * @param a sets a from main
+     * @param b sets b from main
+     * @param c sets c from main
+     * @param d sets d from main
+     * @param e sets e from main
+     * @param f sets f from main
+     * @param g sets g from main
+     */
     public void setNumbers(double a, double b, double c,
                            double d, double e, double f, double g) {
         this.a = a;
@@ -24,18 +31,6 @@ public class Root {
         this.g = g;
     }
 
-    // start with a guessed x0
-    //value which can be randomly generated.
-    /**
-     * Random number generator for initial x value
-     *
-     * @return random value for x_0
-     */
-    public int randX(){
-        Random rand = new Random();
-        return rand.nextInt();
-    }
-
     /**
      * Boolean returns true if root is found else false
      *
@@ -45,10 +40,18 @@ public class Root {
         return false;
     }
 
+    /**
+     *
+     * @param x set x from doMath() for the equation
+     */
     private void setX(double x){
         this.x = x;
     }
 
+    /**
+     *
+     * @return equation with x given to doMath()
+     */
     private double equation(){
         return (a * Math.pow(this.x, 6))-(b * Math.pow(this.x, 5)) +
                 (this.c * Math.pow(this.x, 4))
@@ -57,6 +60,10 @@ public class Root {
                 - (this.f * this.x) + this.g;
     }
 
+    /**
+     *
+     * @return drivatve of the equation with x given to doMath()
+     */
     private double firstDrivative(){
         return ((a * 6) * Math.pow(this.x, 5))
                 - ((b * 5) * Math.pow(this.x, 4))
@@ -64,5 +71,15 @@ public class Root {
                 - ((this.d * 3) * Math.pow(this.x, 2))
                 + ((this.e * 2) * this.x)
                 - this.f;
+    }
+
+    /**
+     *
+     * @param x the x to pass to setX for the formula
+     * @return returns x_n+1 back to main
+     */
+    public double doMath(double x){
+        setX(x);
+        return this.x - (equation()/firstDrivative());
     }
 }
